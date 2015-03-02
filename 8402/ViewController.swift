3639cd97c41df8f705707103b98b36f200c5be42
Ethan Lillie <lillieethan@gmail.com>
2015-03-01 21:00:26 -0800

    Initial Commit

---
+++ 8402/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  8402
+//
+//  Created by Ethan Lillie on 3/1/15.
+//  Copyright (c) 2015 Ethan Lillie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
